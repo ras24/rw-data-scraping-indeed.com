d3b8e0c714dd30d26288752512d8d163d6c41ea6
ras24 <siregarassire@gmail.com>
2022-05-03 12:24:59 +0800

    commit-failed-soup-find

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from bs4 import BeautifulSoup
 url = 'https://www.indeed.com/jobs?'
 params = {
     'q': 'Python Developer',
-    'l': 'Indonesia'
+    'l': 'Indonesia',
+    'vjk': '9f78e01b2c7bfd95'
 }
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'}
 
@@ -14,7 +15,8 @@ res = requests.get(url, params=params, headers=headers)
 def get_total_pages():
     params = {
         'q': 'Python Developer',
-        'l': 'Indonesia'
+        'l': 'Indonesia',
+        'vjk': '9f78e01b2c7bfd95'
     }
     
     res = requests.get(url, params=params, headers=headers)
@@ -30,7 +32,8 @@ def get_total_pages():
         
     # Scraping Step
     soup = BeautifulSoup(res.text, 'html.parser')
-    print(soup.prettify())
+    pagination = soup.find('ul', attrs={'class': 'pagination-list'})
+    print(pagination)
     
 if __name__ == '__main__':
     get_total_pages()
